1bb0d326a8088c7d3ccae6c754a2584349ef71fd
pip64 <76626915+pip64@users.noreply.github.com>
2022-07-08 23:23:59 +0700

    PyBebra

---
+++ shell.py
@@ -0,0 +1,14 @@
+import bebra
+
+while True:
+	text = input('> ')
+	if text.strip() == "": continue
+	result, error = bebra.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
